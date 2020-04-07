24f97537548e0ace21f5ab95fda98c967cea0ed6
Lauren Dove <elledoveinc@gmail.com>
2020-04-07 14:57:57 -0700

    Done.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,14 @@
 class BirdsController < ApplicationController
   def index
-    @birds = Bird.all
-    render json: @birds
+    birds = Bird.all
+    #render json: birds
+    # render json: birds, except: [:created_at, :updated_at] - EVerything except created and updated at.
+    render json: birds, only: [:id, :name, :species] # leaves out updated and created at out.
   end
+
+  def show
+   bird = Bird.find_by(id: params[:id])
+   render json: {id: bird.id, name: bird.name, species: bird.species } # leaves out updated and created at out.
+  end
+
 end
